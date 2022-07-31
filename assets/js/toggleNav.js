30af88a78bbb86d36184fc565e92267fbab92d38
split56 <84617365+split56@users.noreply.github.com>
2022-07-31 10:09:49 +0700

    Update toggleNav.js

--- assets/js/toggleNav.js
+++ assets/js/toggleNav.js
@@ -1,5 +1,26 @@
 const toggle = document.querySelector('.nav-toggle')
-const barOpen=document.querySelector('.nav-inner-container')
+const barOpen=document.querySelector('.nav-list')
+window.addEventListener('resize',()=>{
+let width = screen.width;
+
+    if(width < 570) {
+        barOpen.classList.add('hide')
+    } else {
+        barOpen.classList.remove('hide')
+    }
+})
 toggle.addEventListener('click',function(){
-    barOpen.classList.toggle('show');
+    
+    const scrollHeight=window.pageYOffset;
+    const navHeight=navbar.getBoundingClientRect().height
+    if(scrollHeight<navHeight){
+        navbar.classList.add('fixed')
+    }
+    if(barOpen.classList.contains('show')) {
+        barOpen.classList.remove('show');
+        barOpen.classList.add('hide');
+    } else {
+        barOpen.classList.add('show');
+        barOpen.classList.remove('hide');
+    }
 })
